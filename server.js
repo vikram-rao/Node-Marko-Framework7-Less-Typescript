0b8159ef681f345f62ec909ddd8c795864c9247c
Vikram Rao <vikram.s.rao@gmail.com>
2016-05-10 20:03:24 +0530

    Fixed a bug where init.js - custom js for intializing android or ios styles - was not running

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-//Marko code for serverside processing
 require('marko/node-require').install();
 var lasso = require('lasso');
 lasso.configure("lasso.json");
 
 var serveStatic = require("serve-static");
 
-//Express to server the above processed output
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,5 +2,5 @@ var template = require('marko').load(require('./template.marko'));
 
 module.exports = function(req, res) {
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    template.render({name:"Vikram", buttonName:"Hello"}, res);
+    template.render({}, res);
 };
